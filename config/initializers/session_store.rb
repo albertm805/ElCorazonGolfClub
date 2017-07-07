fa4a0bd4d6395bb4c707b9cdd409bfef67eae7c6
Alberto Mendoza <albertm_805@outlook.com>
2017-07-07 15:57:44 -0500

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Corazongolfo::Application.config.session_store :cookie_store, key: '_corazongolfo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Corazongolfo::Application.config.session_store :active_record_store
